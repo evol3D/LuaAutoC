7b27e4d9dfc64d065aa5ac8d96beaee52299769d
Robear Selwans <robear.selwans@outlook.com>
2021-04-14 13:35:44 +0200

    Added DLL directives
    Signed-off-by: Robear Selwans <robear.selwans@outlook.com>

--- lautoc.h
+++ lautoc.h
@@ -20,13 +20,34 @@
 #include <assert.h>
 
 /*
+** EXPORT/IMPORT
+*/
+#if defined(LAC_DLL)
+  #if defined(_WINDOWS) || defined(_WIN32)
+    #if defined(LAC_IMPL)
+      #define LAC_API __declspec(dllexport)
+    #else
+      #define LAC_API __declspec(dllimport)
+    #endif
+  #elif defined(__linux__) || defined(__FreeBSD__) || defined(__OpenBSD__)
+    #if defined(LAC_IMPL)
+      #define LAC_API __attribute__((visibility("default")))
+    #else
+      #define LAC_API
+    #endif
+  #endif
+#else
+  #define LAC_API
+#endif
+
+/*
 ** Open / Close
 */
 
 #define LUAA_REGISTRYPREFIX "lautoc_"
 
-void luaA_open(lua_State* L);
-void luaA_close(lua_State* L);
+LAC_API void luaA_open(lua_State* L);
+LAC_API void luaA_close(lua_State* L);
 
 /*
 ** Types
@@ -42,11 +63,11 @@ typedef lua_Integer luaA_Type;
 typedef int (*luaA_Pushfunc)(lua_State*, luaA_Type, const void*);
 typedef void (*luaA_Tofunc)(lua_State*, luaA_Type, void*, int);
 
-luaA_Type luaA_type_add(lua_State* L, const char* type, size_t size);
-luaA_Type luaA_type_find(lua_State* L, const char* type);
+LAC_API luaA_Type luaA_type_add(lua_State* L, const char* type, size_t size);
+LAC_API luaA_Type luaA_type_find(lua_State* L, const char* type);
 
-const char* luaA_typename(lua_State* L, luaA_Type id);
-size_t luaA_typesize(lua_State* L, luaA_Type id);
+LAC_API const char* luaA_typename(lua_State* L, luaA_Type id);
+LAC_API size_t luaA_typesize(lua_State* L, luaA_Type id);
 
 /*
 ** Stack
@@ -63,55 +84,55 @@ size_t luaA_typesize(lua_State* L, luaA_Type id);
 #define luaA_conversion_push_registered(L, type) luaA_conversion_push_registered_typ(L, luaA_type(L, type));
 #define luaA_conversion_to_registered(L, type) luaA_conversion_to_registered_type(L, luaA_type(L, type));
 
-int luaA_push_type(lua_State* L, luaA_Type type, const void* c_in);
-void luaA_to_type(lua_State* L, luaA_Type type, void* c_out, int index);
+LAC_API int luaA_push_type(lua_State* L, luaA_Type type, const void* c_in);
+LAC_API void luaA_to_type(lua_State* L, luaA_Type type, void* c_out, int index);
 
-void luaA_conversion_type(lua_State* L, luaA_Type type_id, luaA_Pushfunc push_func, luaA_Tofunc to_func);
-void luaA_conversion_push_type(lua_State* L, luaA_Type type_id, luaA_Pushfunc func);
-void luaA_conversion_to_type(lua_State* L, luaA_Type type_id, luaA_Tofunc func);
+LAC_API void luaA_conversion_type(lua_State* L, luaA_Type type_id, luaA_Pushfunc push_func, luaA_Tofunc to_func);
+LAC_API void luaA_conversion_push_type(lua_State* L, luaA_Type type_id, luaA_Pushfunc func);
+LAC_API void luaA_conversion_to_type(lua_State* L, luaA_Type type_id, luaA_Tofunc func);
 
-bool luaA_conversion_registered_type(lua_State* L, luaA_Type type);
-bool luaA_conversion_push_registered_type(lua_State* L, luaA_Type type);
-bool luaA_conversion_to_registered_type(lua_State* L, luaA_Type type);
+LAC_API bool luaA_conversion_registered_type(lua_State* L, luaA_Type type);
+LAC_API bool luaA_conversion_push_registered_type(lua_State* L, luaA_Type type);
+LAC_API bool luaA_conversion_to_registered_type(lua_State* L, luaA_Type type);
 
-int luaA_push_bool(lua_State* L, luaA_Type, const void* c_in);
-int luaA_push_char(lua_State* L, luaA_Type, const void* c_in);
-int luaA_push_signed_char(lua_State* L,luaA_Type, const void* c_in);
-int luaA_push_unsigned_char(lua_State* L, luaA_Type, const void* c_in);
-int luaA_push_short(lua_State* L, luaA_Type, const void* c_in);
-int luaA_push_unsigned_short(lua_State* L, luaA_Type, const void* c_in);
-int luaA_push_int(lua_State* L, luaA_Type, const void* c_in);
-int luaA_push_unsigned_int(lua_State* L, luaA_Type, const void* c_in);
-int luaA_push_long(lua_State* L, luaA_Type, const void* c_in);
-int luaA_push_unsigned_long(lua_State* L, luaA_Type, const void* c_in);
-int luaA_push_long_long(lua_State* L, luaA_Type, const void* c_in);
-int luaA_push_unsigned_long_long(lua_State* L, luaA_Type, const void* c_in);
-int luaA_push_float(lua_State* L, luaA_Type, const void* c_in);
-int luaA_push_double(lua_State* L, luaA_Type, const void* c_in);
-int luaA_push_long_double(lua_State* L, luaA_Type, const void* c_in);
-int luaA_push_char_ptr(lua_State* L, luaA_Type, const void* c_in);
-int luaA_push_const_char_ptr(lua_State* L, luaA_Type, const void* c_in);
-int luaA_push_void_ptr(lua_State* L, luaA_Type, const void* c_in);
-int luaA_push_void(lua_State* L, luaA_Type, const void* c_in);
+LAC_API int luaA_push_bool(lua_State* L, luaA_Type, const void* c_in);
+LAC_API int luaA_push_char(lua_State* L, luaA_Type, const void* c_in);
+LAC_API int luaA_push_signed_char(lua_State* L,luaA_Type, const void* c_in);
+LAC_API int luaA_push_unsigned_char(lua_State* L, luaA_Type, const void* c_in);
+LAC_API int luaA_push_short(lua_State* L, luaA_Type, const void* c_in);
+LAC_API int luaA_push_unsigned_short(lua_State* L, luaA_Type, const void* c_in);
+LAC_API int luaA_push_int(lua_State* L, luaA_Type, const void* c_in);
+LAC_API int luaA_push_unsigned_int(lua_State* L, luaA_Type, const void* c_in);
+LAC_API int luaA_push_long(lua_State* L, luaA_Type, const void* c_in);
+LAC_API int luaA_push_unsigned_long(lua_State* L, luaA_Type, const void* c_in);
+LAC_API int luaA_push_long_long(lua_State* L, luaA_Type, const void* c_in);
+LAC_API int luaA_push_unsigned_long_long(lua_State* L, luaA_Type, const void* c_in);
+LAC_API int luaA_push_float(lua_State* L, luaA_Type, const void* c_in);
+LAC_API int luaA_push_double(lua_State* L, luaA_Type, const void* c_in);
+LAC_API int luaA_push_long_double(lua_State* L, luaA_Type, const void* c_in);
+LAC_API int luaA_push_char_ptr(lua_State* L, luaA_Type, const void* c_in);
+LAC_API int luaA_push_const_char_ptr(lua_State* L, luaA_Type, const void* c_in);
+LAC_API int luaA_push_void_ptr(lua_State* L, luaA_Type, const void* c_in);
+LAC_API int luaA_push_void(lua_State* L, luaA_Type, const void* c_in);
 
-void luaA_to_bool(lua_State* L, luaA_Type, void* c_out, int index);
-void luaA_to_char(lua_State* L, luaA_Type, void* c_out, int index);
-void luaA_to_signed_char(lua_State* L, luaA_Type,  void* c_out, int index);
-void luaA_to_unsigned_char(lua_State* L, luaA_Type,  void* c_out, int index);
-void luaA_to_short(lua_State* L, luaA_Type,  void* c_out, int index);
-void luaA_to_unsigned_short(lua_State* L, luaA_Type,  void* c_out, int index);
-void luaA_to_int(lua_State* L, luaA_Type,  void* c_out, int index);
-void luaA_to_unsigned_int(lua_State* L, luaA_Type,  void* c_out, int index);
-void luaA_to_long(lua_State* L, luaA_Type,  void* c_out, int index);
-void luaA_to_unsigned_long(lua_State* L, luaA_Type,  void* c_out, int index);
-void luaA_to_long_long(lua_State* L, luaA_Type,  void* c_out, int index);
-void luaA_to_unsigned_long_long(lua_State* L, luaA_Type,  void* c_out, int index);
-void luaA_to_float(lua_State* L, luaA_Type,  void* c_out, int index);
-void luaA_to_double(lua_State* L, luaA_Type,  void* c_out, int index);
-void luaA_to_long_double(lua_State* L, luaA_Type,  void* c_out, int index);
-void luaA_to_char_ptr(lua_State* L, luaA_Type,  void* c_out, int index);
-void luaA_to_const_char_ptr(lua_State* L, luaA_Type,  void* c_out, int index);
-void luaA_to_void_ptr(lua_State* L, luaA_Type,  void* c_out, int index);
+LAC_API void luaA_to_bool(lua_State* L, luaA_Type, void* c_out, int index);
+LAC_API void luaA_to_char(lua_State* L, luaA_Type, void* c_out, int index);
+LAC_API void luaA_to_signed_char(lua_State* L, luaA_Type,  void* c_out, int index);
+LAC_API void luaA_to_unsigned_char(lua_State* L, luaA_Type,  void* c_out, int index);
+LAC_API void luaA_to_short(lua_State* L, luaA_Type,  void* c_out, int index);
+LAC_API void luaA_to_unsigned_short(lua_State* L, luaA_Type,  void* c_out, int index);
+LAC_API void luaA_to_int(lua_State* L, luaA_Type,  void* c_out, int index);
+LAC_API void luaA_to_unsigned_int(lua_State* L, luaA_Type,  void* c_out, int index);
+LAC_API void luaA_to_long(lua_State* L, luaA_Type,  void* c_out, int index);
+LAC_API void luaA_to_unsigned_long(lua_State* L, luaA_Type,  void* c_out, int index);
+LAC_API void luaA_to_long_long(lua_State* L, luaA_Type,  void* c_out, int index);
+LAC_API void luaA_to_unsigned_long_long(lua_State* L, luaA_Type,  void* c_out, int index);
+LAC_API void luaA_to_float(lua_State* L, luaA_Type,  void* c_out, int index);
+LAC_API void luaA_to_double(lua_State* L, luaA_Type,  void* c_out, int index);
+LAC_API void luaA_to_long_double(lua_State* L, luaA_Type,  void* c_out, int index);
+LAC_API void luaA_to_char_ptr(lua_State* L, luaA_Type,  void* c_out, int index);
+LAC_API void luaA_to_const_char_ptr(lua_State* L, luaA_Type,  void* c_out, int index);
+LAC_API void luaA_to_void_ptr(lua_State* L, luaA_Type,  void* c_out, int index);
 
 /*
 ** Structs
@@ -138,26 +159,26 @@ void luaA_to_void_ptr(lua_State* L, luaA_Type,  void* c_out, int index);
 #define luaA_struct_registered(L, type) luaA_struct_registered_type(L, luaA_type(L, type))
 #define luaA_struct_next_member_name(L, type, member) luaA_struct_next_member_name_type(L, luaA_type(L,type), member)
 
-void luaA_struct_type(lua_State* L, luaA_Type type);
-void luaA_struct_member_type(lua_State* L, luaA_Type type, const char* member, luaA_Type member_type, size_t offset);
+LAC_API void luaA_struct_type(lua_State* L, luaA_Type type);
+LAC_API void luaA_struct_member_type(lua_State* L, luaA_Type type, const char* member, luaA_Type member_type, size_t offset);
 
-int luaA_struct_push_type(lua_State* L, luaA_Type type, const void* c_in);
-int luaA_struct_push_member_offset_type(lua_State* L, luaA_Type type, size_t offset, const void* c_in);
-int luaA_struct_push_member_name_type(lua_State* L, luaA_Type type, const char* member, const void* c_in);
+LAC_API int luaA_struct_push_type(lua_State* L, luaA_Type type, const void* c_in);
+LAC_API int luaA_struct_push_member_offset_type(lua_State* L, luaA_Type type, size_t offset, const void* c_in);
+LAC_API int luaA_struct_push_member_name_type(lua_State* L, luaA_Type type, const char* member, const void* c_in);
 
-void luaA_struct_to_type(lua_State* L, luaA_Type type, void* c_out, int index);
-void luaA_struct_to_member_offset_type(lua_State* L, luaA_Type type, size_t offset, void* c_out, int index);
-void luaA_struct_to_member_name_type(lua_State* L, luaA_Type type, const char* member, void* c_out, int index);
+LAC_API void luaA_struct_to_type(lua_State* L, luaA_Type type, void* c_out, int index);
+LAC_API void luaA_struct_to_member_offset_type(lua_State* L, luaA_Type type, size_t offset, void* c_out, int index);
+LAC_API void luaA_struct_to_member_name_type(lua_State* L, luaA_Type type, const char* member, void* c_out, int index);
 
-bool luaA_struct_has_member_offset_type(lua_State* L, luaA_Type type, size_t offset);
-bool luaA_struct_has_member_name_type(lua_State* L, luaA_Type type, const char* member);
+LAC_API bool luaA_struct_has_member_offset_type(lua_State* L, luaA_Type type, size_t offset);
+LAC_API bool luaA_struct_has_member_name_type(lua_State* L, luaA_Type type, const char* member);
 
-luaA_Type luaA_struct_typeof_member_offset_type(lua_State* L, luaA_Type type, size_t offset);
-luaA_Type luaA_struct_typeof_member_name_type(lua_State* L, luaA_Type type, const char* member);
+LAC_API luaA_Type luaA_struct_typeof_member_offset_type(lua_State* L, luaA_Type type, size_t offset);
+LAC_API luaA_Type luaA_struct_typeof_member_name_type(lua_State* L, luaA_Type type, const char* member);
 
-bool luaA_struct_registered_type(lua_State* L, luaA_Type type);
+LAC_API bool luaA_struct_registered_type(lua_State* L, luaA_Type type);
 
-const char* luaA_struct_next_member_name_type(lua_State* L, luaA_Type type, const char* member);
+LAC_API const char* luaA_struct_next_member_name_type(lua_State* L, luaA_Type type, const char* member);
 
 /*
 ** Enums
@@ -176,17 +197,17 @@ const char* luaA_struct_next_member_name_type(lua_State* L, luaA_Type type, cons
 #define luaA_enum_registered(L, type) luaA_enum_registered_type(L, luaA_type(L, type))
 #define luaA_enum_next_value_name(L, type, member) luaA_enum_next_value_name_type(L, luaA_type(L,type), member)
 
-void luaA_enum_type(lua_State* L, luaA_Type type, size_t size);
-void luaA_enum_value_type(lua_State *L, luaA_Type type, const void* value, const char* name);
+LAC_API void luaA_enum_type(lua_State* L, luaA_Type type, size_t size);
+LAC_API void luaA_enum_value_type(lua_State *L, luaA_Type type, const void* value, const char* name);
 
-int luaA_enum_push_type(lua_State *L, luaA_Type type, const void* c_in);
-void luaA_enum_to_type(lua_State* L, luaA_Type type, void *c_out, int index);
+LAC_API int luaA_enum_push_type(lua_State *L, luaA_Type type, const void* c_in);
+LAC_API void luaA_enum_to_type(lua_State* L, luaA_Type type, void *c_out, int index);
 
-bool luaA_enum_has_value_type(lua_State* L, luaA_Type type, const void* value);
-bool luaA_enum_has_name_type(lua_State* L, luaA_Type type, const char* name);
+LAC_API bool luaA_enum_has_value_type(lua_State* L, luaA_Type type, const void* value);
+LAC_API bool luaA_enum_has_name_type(lua_State* L, luaA_Type type, const char* name);
 
-bool luaA_enum_registered_type(lua_State *L, luaA_Type type);
-const char* luaA_enum_next_value_name_type(lua_State* L, luaA_Type type, const char* member);
+LAC_API bool luaA_enum_registered_type(lua_State *L, luaA_Type type);
+LAC_API const char* luaA_enum_next_value_name_type(lua_State* L, luaA_Type type, const char* member);
 
 /*
 ** Functions
@@ -205,9 +226,9 @@ enum {
 
 typedef void (*luaA_Func)(void*, void*);
 
-int luaA_call(lua_State* L, void* func_ptr);
-int luaA_call_name(lua_State* L, const char* func_name);
+LAC_API int luaA_call(lua_State* L, void* func_ptr);
+LAC_API int luaA_call_name(lua_State* L, const char* func_name);
 
-void luaA_function_register_type(lua_State* L, void* src_func, luaA_Func auto_func, const char* name, luaA_Type ret_tid, int num_args, ...);
+LAC_API void luaA_function_register_type(lua_State* L, void* src_func, luaA_Func auto_func, const char* name, luaA_Type ret_tid, int num_args, ...);
 
 #endif
